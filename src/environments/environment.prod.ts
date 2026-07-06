39e2abcb0895e96824425aa1ec60966de84a71b2
BryanZavala <bryan.Dweb@gmail.com>
2026-07-07 00:52:54 +0200

    chore: add RescueGroups config to environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://servidor-protectora-bice.vercel.app',
+  rescueGroupsEnabled: true,
+  rescueGroupsProxyUrl: '/api/rescuegroups',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5002',
+  rescueGroupsEnabled: true,
+  rescueGroupsProxyUrl: '/api/rescuegroups',
 };
